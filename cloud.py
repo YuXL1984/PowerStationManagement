be770a45e115de9870f5a08748bba87c9e191cfc
YuXL1984 <7585269@qq.com>
2016-03-18 15:48:08 +0800

    update

--- cloud.py
+++ cloud.py
@@ -15,34 +15,86 @@ def hello(**params):
         return 'Hello, LeanCloud!'
 
 @engine.define
-def addData(**params):
+#读取所有站点数据
+def loadStationData():
+    funResult = StationData().load_stationData()
+    resultDic = {'code':'1000','resultData':funResult}
+    return resultDic
+
+@engine.define
+#添加站点,传入站点名称和地址,添加后返回站点信息
+def addStationData(**params):
+    #判断传入参数中是否包含站点名称和站点地址
     if 'stationName' in params and 'stationAddress' in params:
-        add_station_data = StationData()
-        add_station_data.set('stationName',params['stationName'])
-        add_station_data.set('stationAddress',params['stationAddress'])
-        station_number = StationData().add_stationData(add_station_data)
-        return station_number
+        funResult = StationData().add_stationData(params['stationName'],params['stationAddress'])
+        if funResult is '101':
+            resultDic = {'code':'1002','error':'站点名称已存在'}
+            return resultDic
+        else:
+            resultDic = {'code':'1000','resultData':funResult}
+            return resultDic
+    else:
+        resultDic = {'code':'1001','error':'传入参数错误'}
+        return resultDic
 
 @engine.define
-def delData(**params):
+def delStationDataForOid(**params):
     if 'objectId' in params:
-        funRequest = StationData().del_stationData(params['objectId'])
-        return funRequest
+        funRequest = StationData().del_stationDataForOid(params['objectId'])
+        if funRequest is '100':
+            resultDic = {'code':'1000','resultData':'删除成功'}
+            return resultDic
+        elif funRequest is '101':
+            resultDic = {'code':'1003','resultData':'objectId不存在'}
+            return resultDic
+        else:
+            resultDic = {'code':'1002','resultData':'删除失败'}
+            return resultDic
+    else:
+        resultDic = {'code':'1001','error':'传入参数错误'}
+        return resultDic
 
 @engine.define
-def searchForId(**params):
-    objectId = params['objectId']
-    funRequest = StationData().searchFromId(objectId)
-    return funRequest
+def searchStationDataForOid(**params):
+    if 'objectId' in params:
+        funResult = StationData().search_stationDataForOid(params['objectId'])
+        if funResult is '101':
+            resultDic = {'code':'1002','error':'搜索无结果'}
+            return resultDic
+        else:
+            resultDic = {'code':'1000','resultData':funResult}
+            return resultDic
+    else:
+        resultDic = {'code':'1001','error':'传入参数错误'}
+        return resultDic
 
 @engine.define
-def readData():
-    funRequest = StationData().readStationData()
-    return funRequest
+def searchStationDataForName(**params):
+    if 'stationName' in params:
+        funResult = StationData().search_stationDataForName(params['stationName'])
+        if funResult is '101':
+            resultDic = {'code':'1002','error':'搜索无结果'}
+            return resultDic
+        else:
+            resultDic = {'code':'1000','resultData':funResult}
+            return resultDic
+    else:
+        resultDic = {'code':'1001','error':'传入参数错误'}
+        return resultDic
 
-
-
-
+@engine.define
+def updateStationDataForOid(**params):
+    if 'objectId'in params and 'newStationName' in params and 'newStationAddress' in params:
+        funResult = StationData().update_stationDataForOid(params['objectId'],params['newStationName'],params['newStationAddress'],)
+        if funResult is '101':
+            resultDic = {'code':'1002','error':'objectId不正确'}
+            return resultDic
+        else:
+            resultDic = {'code':'1000','resultData':funResult}
+            return resultDic
+    else:
+        resultDic = {'code':'1001','error':'传入参数错误'}
+        return resultDic
 
 
 
--- stationdata.py
+++ stationdata.py
@@ -5,38 +5,99 @@ from leancloud import Query
 
 class StationData(Object):
 
-    def add_stationData(self, add_station_data):
-        add_station_data.save()
-        return  add_station_data.get('stationNumber')
-        # return add_station_data.id
+    def load_stationData(self):
+        queryResult = Query.do_cloud_query('select * from StationData')
+        resultObjests = queryResult.results
+        stationData_list = []
+        for object in resultObjests:
+            stationData_list.append((object.id, object.get('stationNumber'), object.get('stationName'), object.get('stationAddress')))
+        station_key = ('objectId', 'stationNumber', 'stationName', 'stationAddress')
+        resultDic = map(lambda x: dict(zip(station_key, x)), stationData_list)
+        return resultDic
 
-    def del_stationData(self,del_station_data):
-        del_station_data.destroy()
-        return '1000'
+    def add_stationData(self, stationName,stationAddress):
+        funResult = StationData().search_stationDataForName(stationName)
+        if funResult is '101':
+            add_station_data = StationData()
+            add_station_data.set('stationName',stationName)
+            add_station_data.set('stationAddress',stationAddress)
+            add_station_data.save()
+            resultDic = {'objectId':add_station_data.id,'stationNumber':add_station_data.get('stationNumber'),'stationName':add_station_data.get('stationName'),'stationAddress':add_station_data.get('stationAddress')}
+            return resultDic
+        else:
+            return '101'
 
-    def searchFromId(self,objectId):
+    def del_stationDataForOid(self,objectId):
+        #搜索oId是否存在
+        funResult = self.search_stationDataForOid(objectId)
+        #!=101代表search_stationDataForOid方法搜索有结果
+        if funResult != '101':
+            #删除该对象
+            resultData = Query.do_cloud_query('delete from StationData where objectId = ?',objectId)
+            #删除后再次搜索
+            funResult = self.search_stationDataForOid(objectId)
+            #如果搜索无结果
+            if funResult is '101':
+                #返回删除成功
+                return '100'#成功,搜索无结果
+            else:
+                return '102'#失败,搜索有结果
+        else:
+            #objectId没有找到
+            return '101'
+
+    def search_stationDataForOid(self,objectId):
+        #使用CQL语句查询
+        queryResult = Query.do_cloud_query('select * from StationData where objectId = ?',objectId)
+        #接收查询结果
+        resultObjests = queryResult.results
+        #如果结果存在
+        if resultObjests:
+            #将结果保存至resultDic
+            stationData_list = []
+            for object in resultObjests:
+                stationData_list.append((object.id, object.get('stationNumber'), object.get('stationName'), object.get('stationAddress')))
+            station_key = ('objectId', 'stationNumber', 'stationName', 'stationAddress')
+            resultDic = map(lambda x: dict(zip(station_key, x)), stationData_list)
+            return resultDic
+        else:
+            return '101'
+
+    def search_stationDataForName(self,stationName):
         query = Query(StationData)
-        data_score = query.get(objectId)
-        requestDic = {'stationNumber':data_score.get('stationNumber'),'stationName':data_score.get('stationName'),'stationAddress':data_score.get('stationAddress')}
-        return requestDic
+        #查询条件
+        query.equal_to('stationName',stationName)
+        #接收查询结果
+        resultObjests = query.find()
+        #如果结果存在
+        if resultObjests:
+            #将结果保存至resultDic
+            stationData_list = []
+            for object in resultObjests:
+                stationData_list.append((object.id, object.get('stationNumber'), object.get('stationName'), object.get('stationAddress')))
+            station_key = ('objectId', 'stationNumber', 'stationName', 'stationAddress')
+            resultDic = map(lambda x: dict(zip(station_key, x)), stationData_list)
+            return resultDic
+        else:
+            return '101'
 
-    def readStationData(self):
-        result = Query.do_cloud_query('select * from StationData')
-        # results 是查询返回的结果，Object 列表
-        results = result.results
-        station_list = []
-        for item in results:
-            station_list.append((item.get('objectID'),item.get('stationNumber'), item.get('stationName'), item.get('stationAddress')))
-        station_key = ('objectID','stationNumber', 'stationName', 'stationAddress')
-        requestDic = map(lambda x: dict(zip(station_key, x)), station_list)
-        return requestDic
+    def update_stationDataForOid(self,objectId,newStationName,newStationAddress):
+        #根据Oid查询目前值
+        funResult = self.search_stationDataForOid(objectId)
+        resultDic = funResult[0]
+        if funResult != '101':
+            #判断新站点名称是否改变
+            if resultDic['stationName'] != newStationName:
+                #如果改变进行更新
+                queryResult = Query.do_cloud_query('update StationData set stationName = ?  where objectId = ?',newStationName,objectId)
+            #判断新站点地址是否改变
+            if resultDic['stationAddress'] != newStationAddress:
+                #如果改变进行更新
+                queryResult = Query.do_cloud_query('update StationData set stationAddress = ?  where objectId = ?',newStationAddress,objectId)
+            funResult = self.search_stationDataForOid(objectId)
+            #返回更新后的值
+            return funResult
+        else:
+            #找不到Oid
+            return '101'
 
-
-
-
-
-
-
-
-
-#    def update_stationData(self):
