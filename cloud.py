523e68f2c167eba010919ff359d304863a76a367
YuXL1984 <7585269@qq.com>
2016-03-21 15:50:31 +0800

    update

--- cloud.py
+++ cloud.py
@@ -21,7 +21,7 @@ def loadStationData():
     print '===========> loadStationData'
     funResult = StationData().load_stationData()
     resultDic = {'code':'1000','resultStationData':funResult}
-    return funResult
+    return resultDic
 
 @engine.define
 def loadSectionStationData(**params):
